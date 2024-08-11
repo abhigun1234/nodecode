52af9de49e989d00e4038600994f8764c195cdb6
abhigun1234 <37846168+abhigun1234@users.noreply.github.com>
2024-08-11 11:41:39 +0530

    add

--- August_2024/expressdemo.js
+++ August_2024/expressdemo.js
@@ -7,3 +7,18 @@ app.listen(2004,function()
 
     console.log('listining at port  2004')
 })
+app.get('/',(req,res)=>{
+    res.send('hello everyone how are you')
+})
+app.get('/hello',(req,res)=>{
+    res.send('hello world')
+})
+app.get('/person',(req,res)=>{
+
+    jsonObj=[{"id":1,"first_name":"Lesly","last_name":"Pomeroy","email":"lpomeroy0@w3.org","gender":"Female"},
+        {"id":2,"first_name":"Whitney","last_name":"Bamell","email":"wbamell1@zdnet.com","gender":"Female"},
+        {"id":3,"first_name":"Sibbie","last_name":"Lewington","email":"slewington2@storify.com","gender":"Genderqueer"},
+        {"id":4,"first_name":"Forrester","last_name":"O'Carrol","email":"focarrol3@cocolog-nifty.com","gender":"Male"},
+        {"id":5,"first_name":"Roseanna","last_name":"Welsby","email":"rwelsby4@uol.com.br","gender":"Polygender"}]
+        res.send(jsonObj)
+})
